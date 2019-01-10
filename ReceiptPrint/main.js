a36dcc7fdf6ac0f8b53935d96988420457d92327
Eished <212179840@qq.com>
2019-01-10 09:40:16 +0800

    增加预置数据

--- ReceiptPrint/main.js
+++ ReceiptPrint/main.js
@@ -16,12 +16,24 @@
       { label: "棉棒", category: "配件", unit:"个/包", price:"10", acreage:"" },
       { label: "墨垫", category: "配件", unit:"个/包", price:"10", acreage:"" },
       { label: "光栅", category: "配件", unit:"个/包", price:"10", acreage:"" },
-      { label: "0.914无底纸光膜", category: "无底纸膜", unit:"卷", price:"1.5", acreage:"123" },
-      { label: "0.914无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1.5", acreage:"234" },
-      { label: "1.07无底纸光膜", category: "无底纸膜", unit:"卷", price:"1.5", acreage:"153" },
+      { label: "0.82无底纸光膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"164" },
+      { label: "0.82无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"164" },
+      { label: "0.914无底纸光膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"182.8" },
+      { label: "0.914无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"182.8" },
+      { label: "1.07无底纸光膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"214" },
+      { label: "1.07无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"214" },
+      { label: "1.27无底纸光膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"254" },
+      { label: "1.27无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"254" },
+      { label: "1.37无底纸光膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"274" },
+      { label: "1.37无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"274" },
+      { label: "1.52无底纸光膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"304" },
+      { label: "1.52无底纸哑膜", category: "无底纸膜", unit:"卷", price:"1", acreage:"304" },
       { label: "户外墨水", category: "墨水", unit:"瓶", price:"100", acreage:"" },
-      { label: "户内墨水", category: "墨水", unit:"瓶", price:"100", acreage:"" },
-      { label: "UV墨水", category: "墨水", unit:"瓶", price:"100", acreage:"" }
+      { label: "户内墨水", category: "墨水", unit:"瓶", price:"50", acreage:"" },
+      { label: "UV墨水硬墨", category: "墨水", unit:"瓶", price:"500", acreage:"" }
+      { label: "UV墨水软墨", category: "墨水", unit:"瓶", price:"500", acreage:"" }
+      { label: "清洗液", category: "墨水", unit:"瓶", price:"100", acreage:"" },
+      { label: "UV清洗液", category: "墨水", unit:"瓶", price:"100", acreage:"" },
     ];
 
 
@@ -157,6 +169,7 @@ document.onkeydown=function(event){
          // $("save_btn").click();
          //使用回车键读取数据库数据
          auto();
+         calc();
     }
 };
 
@@ -172,7 +185,6 @@ var view = View.onclick = function(){
 
   //遍历8行表格内容
   var $len = $("td > input").length / 6 ;
-  console.log($len);
   for(; $v < parseInt($len); $v++){
   $(".label")[$v + 8].innerText = $(".label > input")[$v].value;
   $(".unit")[$v + 8].innerText = $(".unit > input")[$v].value;
@@ -201,45 +213,41 @@ var auto = function(){
   //for循环读取input值，累加；
   var len1 = inputs.length;//9行每行循环一次
   var i1 = 3;//跳过前3格
-  console.log("len1" + len1);
     for(; i1 < len1; i1++){
     var input = inputs[i1];
     var b = input.value;
     //遍历数组 如果数据匹配则一次输入数据值
     var len2 = data.length;
     var i2 = 0;
-    console.log(len2);
+    // console.log(len2);
       for(; i2 < len2; i2++){
         var c = data[i2].label;
-        console.log(c + "data");
         if(b == c){
           i1++;
-          console.log(i1+"单位");
+          // console.log(i1+"单位");
           inputs[i1].value = data[i2].unit;
           i1++;
-          console.log(i1+"数量");
+          // console.log(i1+"数量");
           var quantity = inputs[i1].value;
           i1++;
-          console.log(i1+"单价");
+          // console.log(i1+"单价");
           inputs[i1].value = data[i2].price;
           var price = inputs[i1].value;
           i1++;
-          console.log(i1+"金额");
+          // console.log(i1+"金额");
           var money = 0;
           i1++;
-          console.log(i1+"平方");
+          // console.log(i1+"平方");
           inputs[i1].value = data[i2].acreage;
           var acreage = inputs[i1].value;
           //判断备注是否是数字
           if (Number(acreage)) {
             money = Number(quantity) * Number(price) * Number(acreage);
-            console.log(money);
             i1--;
             inputs[i1].value = Number(money.toFixed(f));
             i1++;
           }else {
             money = Number(quantity) * Number(price);
-            console.log(money);
             i1--;
             inputs[i1].value = Number(money.toFixed(f));
             i1++;
@@ -262,21 +270,17 @@ var calc = function(){
   var all_money = 0;
   for(; $i < $len; $i = $i + 3){
       var $quantity = $("input")[$i].value;//数量quantity
-      console.log($quantity);
       $i++;
       var $price = $("input")[$i].value;//单价price
-      console.log($price);
       $i++
       var $money = $("input")[$i].value;//金额money
-      console.log($money);
       $i++
       var $acreage = $("input")[$i].value;//平方acreage
-      console.log($acreage);
       //计算数量*单价*平方
       //判断备注是否是数字
       if (Number($acreage)) {
         $money = Number($quantity) * Number($price) * Number($acreage);
-        console.log($money);
+        // console.log($money);
         $i--;
         $("input")[$i].value = Number($money.toFixed(f));
         $i++;
@@ -285,7 +289,6 @@ var calc = function(){
         $(".remark")[$r + 8].innerText = $("input")[$i].value + "平方米 200米";
       }else{
       $money = Number($quantity) * Number($price);
-      console.log($money);
         if ($money > 0) {
           $i--;
           $("input")[$i].value = Number($money.toFixed(f));
@@ -293,16 +296,14 @@ var calc = function(){
         }
       }
     all_money = all_money + $money;
-    console.log(all_money + "all");
   }
   var $m = 0;
-  $(".all_money")[$m].innerText = all_money.toFixed(f_money);
+  for(; $m < 2; $m++){
+    $(".all_money")[$m].innerText = all_money.toFixed(f_money);
     //调用数字金额大写转换填入指定单元格
-  $(".capitals")[$m].innerText = digitUppercase(all_money.toFixed(f_money)); 
-  $m++;
-  $(".all_money")[$m].innerText = all_money.toFixed(f_money);
-    //调用数字金额大写转换填入指定单元格
-  $(".capitals")[$m].innerText = digitUppercase(all_money.toFixed(f_money)); 
+    $(".capitals")[$m].innerText = digitUppercase(all_money.toFixed(f_money)); 
+  }
+  
 }
       
  
